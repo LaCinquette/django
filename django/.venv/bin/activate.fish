c417c6d81d8e88a81f5749decbe96c3d06169c6b
lacinquette <yurik_sevastyanov123@mail.ru>
2023-04-30 18:55:50 +0300

    ready, all fixed

--- django/.venv/bin/activate.fish
+++ django/.venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/lacinquette/ITMO/diploma/final/prod/django/.venv"
+set -gx VIRTUAL_ENV "/home/lacinquette/ITMO/diploma/final_test/django/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
