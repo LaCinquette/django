1c937370706845255ff261b693d3abb4a59fdc9e
lacinquette <yurik_sevastyanov123@mail.ru>
2023-05-01 14:13:52 +0300

    prettify

--- django/app/settings.py
+++ django/app/settings.py
@@ -137,7 +137,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost:1337"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
