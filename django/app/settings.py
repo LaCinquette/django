b33742c19e863fd43921a527d832d98c4b9d2a47
lacinquette <yurik_sevastyanov123@mail.ru>
2023-05-03 15:14:25 +0300

    fixed settings for local network

--- django/app/settings.py
+++ django/app/settings.py
@@ -137,7 +137,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ["http://localhost"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://192.168.0.3"]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
