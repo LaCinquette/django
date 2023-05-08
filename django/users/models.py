a2c63f464e7f2088a8a67c457a5821cc83349d2f
lacinquette <yurik_sevastyanov123@mail.ru>
2023-05-08 18:45:39 +0300

    fixes

--- django/users/models.py
+++ django/users/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
 
 class Profile(models.Model):
